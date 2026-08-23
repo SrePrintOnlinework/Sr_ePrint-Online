fd604910f42bc55a35d97ea6d702fff27ee3897e
SrePrintOnlinework <sronline99890@gmail.com>
2026-08-23 16:53:22 +0530

    Add PDF data structure with sample entries

---
+++ app/pdf.js
@@ -0,0 +1,12 @@
+export const pdfs = [
+  {
+    id: 'raj-001',
+    name: 'Raj Birth Certificate',
+    file: 'raj-birthcertificate2.pdf',
+  },
+  {
+    id: 'raj-002',
+    name: 'Sample PDF 2',
+    file: 'sample2.pdf',
+  },
+];
