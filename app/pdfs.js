efefc770c142afbd1870c9f23bcba343acf2c58d
Sr_ePrint_Onlinwork <sronline99890@gmail.com>
2026-08-30 22:56:53 +0530

    Update PDF names and add price property

--- app/pdfs.js
+++ app/pdfs.js
@@ -1,17 +1,39 @@
 export const pdfs = [
   {
     id: 'raj-001',
-    name: 'Raj Birth Certificate',
+    name: 'Birth Application PDF',
     file: 'raj-birthcertificate2.pdf',
+    price: 99,
   },
+
   {
     id: 'raj-002',
     name: 'Sample PDF 2',
     file: 'sample2.pdf',
+    price: 99,
   },
-    {
+
+  {
     id: 'raj-003',
     name: 'Sample PDF 3',
     file: 'newdoc08112026.pdf',
+    price: 99,
   },
+
+  // Add more PDFs below
+
+  // {
+  //   id: 'raj-004',
+  //   name: 'PDF Name 4',
+  //   file: 'pdf4.pdf',
+  //   price: 99,
+  // },
+
+  // {
+  //   id: 'raj-005',
+  //   name: 'PDF Name 5',
+  //   file: 'pdf5.pdf',
+  //   price: 99,
+  // },
+
 ];
